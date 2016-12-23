8629a8761cafcb41e2a7172dbaa21cf491167578
Filip Zawada <me@filimanjaro.com>
2016-12-23 01:04:00 +0100

    Add unfailable logic test

--- Tests/CorePromise/06_UnfailablePromise.swift
+++ Tests/CorePromise/06_UnfailablePromise.swift
@@ -42,5 +42,33 @@ class UnfailablePromiseTests: XCTestCase {
 
         waitForExpectations(timeout: 1.0)
     }
+
+    /// test without assertion, but they may fail if compiling should fail on warning
+    func testLogic() {
+        // unfailable + unfailable #1
+        Promise<Int>.with(5).then { val in
+                    return Promise(value: val + 10)
+                }
+
+        // unfailable + unfailable #2
+        Promise<Int>.with(5).then { (val: Int) -> Int in
+                    return val + 10
+                }
+
+        // unfailable + failable #1 -> warning
+        _ = Promise<Int>.with(5).then { val throws -> Int in
+            return val + 5
+        }
+
+        // failable + unfailable -> warning
+        _ = Promise<Int>.with(5).then { val throws -> Int in
+            return val + 5
+        }
+
+        // failable + failable -> warning
+        _ = Promise(value: 5).then { val throws -> Int in
+            return val + 5
+        }
+    }
 }
 
